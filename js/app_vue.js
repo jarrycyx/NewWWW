380e2b442c38a8a7862649052b7e5e1aa4848607
jarrycyx <chengyx18@mails.tsinghua.edu.cn>
2019-06-29 17:06:57 +0800

    fdsf

--- js/app_vue.js
+++ js/app_vue.js
@@ -1,13 +1,13 @@
 new Vue({
     el: '#earncard',
     data: {
-        earncard_message: 'Hello Vue!'
+        candle_message: 'OFF'
     },
     methods: {
         earncard_ChangeMessage: function() {
             thisVue = this;
             var jqxhr = $.get('vue', function(data, status) {
-                thisVue.earncard_message = 'Hello LaraBase4!' + data;
+                thisVue.candle_message = 'ON' + data;
             });
         }
     }
