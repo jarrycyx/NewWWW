d6c78d3f0fed299564a9fa5b7dd0b599ae733d68
jarrycyx <chengyx18@mails.tsinghua.edu.cn>
2019-06-30 15:35:16 +0800

    fafw

--- js/app_vue.js
+++ js/app_vue.js
@@ -22,7 +22,20 @@ var todolistVue = new Vue({
 
 });
 
-var jqxhr = $.get('todolist/date', function(data, status) {
-    //data = `[{"createTime":1561873087985,"scheTime":1561873087985,"content":"一个待办事项0","status":0},{"createTime":1561873087989,"scheTime":1561879087989,"content":"一个待办事项1","status":0},{"createTime":1561873087990,"scheTime":1561885087990,"content":"一个待办事项2","status":0},{"createTime":1561873087990,"scheTime":1561891087990,"content":"一个待办事项3","status":0},{"createTime":1561873087991,"scheTime":1561897087991,"content":"一个待办事项4","status":0},{"createTime":1561873087992,"scheTime":1561903087992,"content":"一个待办事项5","status":0},{"createTime":1561873087993,"scheTime":1561909087993,"content":"一个待办事项6","status":0},{"createTime":1561873087994,"scheTime":1561915087994,"content":"一个待办事项7","status":0},{"createTime":1561873087994,"scheTime":1561921087994,"content":"一个待办事项8","status":0},{"createTime":1561873087995,"scheTime":1561927087995,"content":"一个待办事项9","status":0}]`;
-    todolistVue.items = JSON.parse(data).slice(0, 6);
+$.ajax({
+    url: 'todolist/date',
+    type: 'get',
+    headers: {
+        'Authorization': 'APPCODE eef1833dbc5748cc86a6419ea0f92d9a'
+    },
+    //data : 'type=shehui',
+    //dataType : 'json',
+    success: function(msg) {
+        todolistVue.items = JSON.parse(msg).slice(0, 6);
+    },
+    error: function(err) {
+        data = `[{"createTime":1561873087985,"scheTime":1561873087985,"content":"一个待办事项0","status":0},{"createTime":1561873087989,"scheTime":1561879087989,"content":"一个待办事项1","status":0},{"createTime":1561873087990,"scheTime":1561885087990,"content":"一个待办事项2","status":0},{"createTime":1561873087990,"scheTime":1561891087990,"content":"一个待办事项3","status":0},{"createTime":1561873087991,"scheTime":1561897087991,"content":"一个待办事项4","status":0},{"createTime":1561873087992,"scheTime":1561903087992,"content":"一个待办事项5","status":0},{"createTime":1561873087993,"scheTime":1561909087993,"content":"一个待办事项6","status":0},{"createTime":1561873087994,"scheTime":1561915087994,"content":"一个待办事项7","status":0},{"createTime":1561873087994,"scheTime":1561921087994,"content":"一个待办事项8","status":0},{"createTime":1561873087995,"scheTime":1561927087995,"content":"一个待办事项9","status":0}]`;
+        todolistVue.items = JSON.parse(data).slice(0, 6);
+    }
+
 });
